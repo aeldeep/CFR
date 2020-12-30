6198b5d90d50b903c541964cacefae729bea0003
aeldeep <eng.ahmed-eldeep@hotmail.com>
2020-12-30 02:34:39 -0500

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegisterNewUser.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User U)
+        {
+            if(ModelState.IsValid)
+            {
+                using(UsersEntities us = new UsersEntities())
+                {
+                    us.Users.Add(U);
+                    us.SaveChanges();
+                    ModelState.Clear();
+                    U = null;
+                    ViewBag.Message = "User Registered Successfully";
+                }
+            }
+            return View(U);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RegisterNewUser.MvcApplication" Language="C#" %>
